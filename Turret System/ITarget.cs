ec294fbc69aa7447f830f1995df18ac5aa28ab80
ZhanYi224 <ZhanYi224@users.noreply.github.com>
2020-10-21 11:38:53 +0800

    Create ITarget.cs

---
+++ Turret System/ITarget.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITarget
+{
+    //Do something that let the overlap sphere get the target
+    void TurretTarget();
+}

5a908578db45e7e24351e41a7c687779023135b0
ZhanYi224 <ZhanYi224@users.noreply.github.com>
2020-10-21 11:38:25 +0800

    Create DistanceCompare.cs

---
+++ Turret System/DistanceCompare.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCompare : IComparer
+{
+    private Transform compareTransform;
+
+    public DistanceCompare(Transform compTransform)
+    {
+        compareTransform = compTransform;
+    }
+
+    public int Compare(object x, object y)
+    {
+        Collider xCollider = x as Collider;
+        Collider YCollider = y as Collider;
+
+        Vector3 offset = xCollider.transform.position - compareTransform.position;
+        float xDistance = offset.sqrMagnitude;
+
+        offset = YCollider.transform.position - compareTransform.position;
+        float yDistance = offset.sqrMagnitude;
+
+        return xDistance.CompareTo(yDistance);
+    }
+
+    
+}

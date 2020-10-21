4c06e48f6c0f9da3ebba7ff05fcfe1ef2fc27ce8
ZhanYi224 <ZhanYi224@users.noreply.github.com>
2020-10-21 11:38:15 +0800

    Create Car.cs

---
+++ Turret System/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour, IDamageable
+{
+    [SerializeField]private int maxHealth = 10;
+    [SerializeField] private int health;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void damageable(int damage)
+    {
+        health -= damage;
+        if (health == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}

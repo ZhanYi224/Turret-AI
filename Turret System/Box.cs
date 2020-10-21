0913674dbd9bdea4bfbd05f1ffb7bd5191d04ea9
ZhanYi224 <ZhanYi224@users.noreply.github.com>
2020-10-21 11:37:45 +0800

    Create Box.cs

---
+++ Turret System/Box.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour, IDamageable
+{
+    public void damageable(int damage)
+    {
+        Destroy(this.gameObject);
+    }
+
+}

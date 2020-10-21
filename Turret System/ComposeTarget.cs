c0b143c1b682e1ca4f665ec662366cda91f46181
ZhanYi224 <ZhanYi224@users.noreply.github.com>
2020-10-21 11:38:20 +0800

    Create ComposeTarget.cs

---
+++ Turret System/ComposeTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComposeTarget : MonoBehaviour, ITarget
+{
+    [Header("Changable Attribute")]
+    [SerializeField] private GameObject targetableGameObjects;
+
+    //private GameObject[] targetableObject;
+    //use other way to save it
+    public void TurretTarget()
+    {
+        //targetableGameObjects = this.gameObject;
+
+        //for (int i = 0; i < targetableGameObjects.Length; i++)
+        //{
+        //    GameObject targetableGameobject = targetableGameObjects[i];
+        //    ITarget targetable = targetableGameobject.GetComponent<ITarget>();
+        //    if (targetable == null)
+        //    {
+        //        return;
+        //    }
+        //    targetable.TurretTarget();
+        //}
+
+    }
+}

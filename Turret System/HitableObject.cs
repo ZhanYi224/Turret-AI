56a49764dea19a17879a17726af15aeb14e80fb9
ZhanYi224 <ZhanYi224@users.noreply.github.com>
2020-10-21 11:38:44 +0800

    Create HitableObject.cs

---
+++ Turret System/HitableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitableObject : MonoBehaviour, IDamageable
+{
+    [Header("Changable attribute")]
+    public int maxHealth;
+
+    [Header("Show casing attribute")]
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void damageable(int damage)
+    {
+        health -= damage;
+        if (health == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
